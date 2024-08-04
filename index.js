b8bce3a48d91e60f849a8d8e25558a85455a09a9
baileyjarvis2814 <baileyjarvis2814@gmail.com>
2024-08-04 15:11:12 -0600

    CORS

--- index.js
+++ index.js
@@ -3,29 +3,26 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'X-Requested-With, Content-Type, Accept'
+}));
 
-// Log all incoming requests
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
+app.use(express.json());
 
 const data = require('./OnePieceArcs.json');
 
 app.get('/api/data', (req, res) => {
-  console.log('Sending data...');
   res.json(data);
 });
 
-app.get('/api/data/:arc', (req, res) => {
+app.get('/api/data/arc', (req, res) => {
   const { arc } = req.params;
   const item = data.find((d) => d.Arc.toLowerCase() === arc.toLowerCase());
   if (item) {
     res.json(item);
   } else {
-    console.log('Item not found:', arc);
     res.status(404).send({ error: 'Item not found' });
   }
 });
