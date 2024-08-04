a58e10fb52bf17a86c73e1ccaaa78dcde9f48131
baileyjarvis2814 <baileyjarvis2814@gmail.com>
2024-08-04 15:19:10 -0600

    CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan'); // Morgan for request logging
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,16 +10,19 @@ app.use(cors({
   allowedHeaders: 'X-Requested-With, Content-Type, Accept'
 }));
 
+app.use(morgan('combined')); // Use morgan for logging
 app.use(express.json());
 
 const data = require('./OnePieceArcs.json');
 
 app.get('/api/data', (req, res) => {
+  console.log('Request Headers:', req.headers);
   res.json(data);
 });
 
 app.get('/api/data/arc', (req, res) => {
   const { arc } = req.params;
+  console.log('Request Headers:', req.headers);
   const item = data.find((d) => d.Arc.toLowerCase() === arc.toLowerCase());
   if (item) {
     res.json(item);
