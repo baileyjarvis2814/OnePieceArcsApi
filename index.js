914c84ad2d721b60700275b8a1e87421bfd2b943
baileyjarvis2814 <baileyjarvis2814@gmail.com>
2024-08-04 13:31:02 -0600

    Add CORS support

--- index.js
+++ index.js
@@ -6,18 +6,26 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Log all incoming requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 const data = require('./OnePieceArcs.json');
 
 app.get('/api/data', (req, res) => {
+  console.log('Sending data...');
   res.json(data);
 });
 
-app.get('/api/data/arc', (req, res) => {
+app.get('/api/data/:arc', (req, res) => {
   const { arc } = req.params;
   const item = data.find((d) => d.Arc.toLowerCase() === arc.toLowerCase());
   if (item) {
     res.json(item);
   } else {
+    console.log('Item not found:', arc);
     res.status(404).send({ error: 'Item not found' });
   }
 });
